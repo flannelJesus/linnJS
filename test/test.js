f9b6143bf93ce35296d7e702970ba353ee58f0ed
Kremlin <sjoshuaross@yahoo.com>
2015-05-24 19:07:02 +0100

    added test

---
+++ test/test.js
@@ -0,0 +1,13 @@
+var linnJS = require('../index');
+
+//linnjs.initialize(process.env.linnworksusername, process.env.linnworkspw);
+linnJS.initialize(process.env.linnworksusername, process.env.linnworkspw)
+    .then(function(api){
+        api.post('ImportExport/AreExportsEnabled')
+            .then(function(response){
+                console.log(response);
+            });
+    })
+    .catch(function(err){
+        console.log(err);
+    });
